0b26f3e7a78c72cd800dd1f45d2fbe3e1464c085
Apollo <a.meijer@azerion.com>
2020-04-05 15:20:17 +0200

    Removed statis references in ServiceLocatorManager.cs to make it easier to use it in tests and other places in the project.

--- Assets/Packages/ServiceLocator/Tests/ServiceLocatorTests.cs
+++ Assets/Packages/ServiceLocator/Tests/ServiceLocatorTests.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using ServiceLocator;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -11,12 +12,20 @@ namespace Tests
 {
     public class ServiceLocatorTests
     {
+        private ServiceLocatorManager serviceLocatorManager;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            serviceLocatorManager = new ServiceLocatorManager();
+        }
+        
         [Test]
         public void CreateNonMonoServiceAndResolveIt()
         {
-            ServiceLocatorManager.Register<IMockInterface>(new MockServiceOldFeature());
+            serviceLocatorManager.Register<IMockInterface>(new MockServiceOldFeature());
 
-            IMockInterface oldService = ServiceLocatorManager.Resolve<IMockInterface>();
+            IMockInterface oldService = serviceLocatorManager.Resolve<IMockInterface>();
             Assert.IsInstanceOf(typeof(MockServiceOldFeature), oldService);
         }
         
@@ -24,42 +33,42 @@ namespace Tests
         public void CreateNonMonoServiceDeleteItAndResolveIt()
         {
             Debug.unityLogger.logEnabled = false;
-            ServiceLocatorManager.Register<IMockInterface>(new MockServiceOldFeature());
+            serviceLocatorManager.Register<IMockInterface>(new MockServiceOldFeature());
 
-            ServiceLocatorManager.Reset();
+            serviceLocatorManager.Reset();
 
-            IMockInterface service = ServiceLocatorManager.Resolve<IMockInterface>();
+            IMockInterface service = serviceLocatorManager.Resolve<IMockInterface>();
             Assert.IsNull(service);
         }
         
         [Test]
         public void CreateMonoServiceAndResolveIt()
         {
-            ServiceLocatorManager.Register<IMockInterface>(ServiceLocatorManager.AsMono<MonoMockServiceFeature>());
+            serviceLocatorManager.Register<IMockInterface>(ServiceLocatorManager.AsMono<MonoMockServiceFeature>());
             
-            IMockInterface service = ServiceLocatorManager.Resolve<IMockInterface>();
+            IMockInterface service = serviceLocatorManager.Resolve<IMockInterface>();
             Assert.IsInstanceOf(typeof(MonoMockServiceFeature), service);
         }
         
-        [UnityTest]
-        public IEnumerator CreateMonoServiceResetItAndResolveIt()
+        [Test]
+        public void CreateMonoServiceResetItAndResolveIt()
         {
             Debug.unityLogger.logEnabled = false;
 
-            ServiceLocatorManager.Register<IMockInterface>(ServiceLocatorManager.AsMono<MonoMockServiceFeature>());
+            serviceLocatorManager.Register<IMockInterface>(ServiceLocatorManager.AsMono<MonoMockServiceFeature>());
             
-            ServiceLocatorManager.Reset();
+            serviceLocatorManager.Reset();
 
-            IMockInterface service = ServiceLocatorManager.Resolve<IMockInterface>();
-            Assert.IsNull(service);
-            yield return new WaitForEndOfFrame();
+            IMockInterface service = serviceLocatorManager.Resolve<IMockInterface>();
             var obj = Object.FindObjectOfType<MonoMockServiceFeature>();
             Assert.IsNull(obj);
+            
         }
 
-        [UnitySetUp]
+        [UnityTearDown]
         public IEnumerator AfterEachTest()
         {
+            serviceLocatorManager.Reset();
             Debug.unityLogger.logEnabled = true;
             yield break;
         }
