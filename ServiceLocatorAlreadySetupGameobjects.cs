490c32d592e917e50c819c3463bddc6681aae85a
Emmanuel Omene <>
2022-05-16 08:11:39 +0200

    Added ability to register already setup gameobjects as a service

---
+++ ServiceLocatorAlreadySetupGameobjects.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocatorAlreadySetupGameobjects
+{
+    public GameObject ObjectToInstantiate;
+    public Transform Parent;
+}
